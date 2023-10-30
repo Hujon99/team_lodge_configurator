cfec33bfa6db78bbd2c6550442bd970f226b2680
Hujon99 <joensson.hugo@gmail.com>
2023-10-30 14:09:16 +0100

    Feat: install tailwind css and fontawesome

--- product-configurator/src/main.jsx
+++ product-configurator/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
+import './tailwind.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
